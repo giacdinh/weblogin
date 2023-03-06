b05b9ba1867c04e1b3f63968883964988becaf26
ghecu <ghecu@hotmail.com>
2023-03-06 10:04:41 -0500

    change dbase connect

--- config.php
+++ config.php
@@ -1,5 +1,8 @@
 <?php
-   $con = mysqli_connect('localhost','ghecu','giacde','Robert');
-   if(!$con)
-	echo "Database connect failed";
+$conn=mysqli_connect("localhost","u426042465_streetlight","An940822?","u426042465_streetlight");
+if(!$conn) {
+    echo "DBase connect failed";
+    die("Connection failed: " . mysqli_connect_error());
+    echo "DBase connect failed";
+}
 ?>
